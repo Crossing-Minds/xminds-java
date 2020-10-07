b210af4ad787dab0f976e4fa0199ece3afdda339
Julio Briceno <ing.juliob@gmail.com>
2020-10-07 12:48:35 -0300

    Changes to apply suggestion for user agent in the requests Related to: Actually could you replace "CrossingMinds/" + API_VERSION by "CrossingMinds/" + CLIENT_VERSION where CLIENT_VERSION will be the version of the java package?

--- xminds/src/main/java/com/crossingminds/xminds/api/utils/Constants.java
+++ xminds/src/main/java/com/crossingminds/xminds/api/utils/Constants.java
@@ -5,15 +5,17 @@ public class Constants {
 	/**
 	 * Related to Crossing Mids API
 	 */
-	public static final String API_URL = "https://staging-api.crossingminds.com/";
-	public static final String API_VERSION = "v1";
+	public static final String API_URL = "https://api.crossingminds.com/";
+
+	private Constants() {
+	}
 
 	/*
 	 * Related to Xminds Java API
 	 * 
 	 * @parameter expression="${project.version}"
 	 */
-	private static String JAVA_VERSION;
+	private static String clientVersion;
 
 	/*
 	 * Related to the timeout of the HttpRequest in minutes
@@ -23,8 +25,7 @@ public class Constants {
 	 * Related to Headers
 	 */
 	public static final String HEADER_USER_AGENT = "User-Agent";
-	public static final String HEADER_USER_AGENT_VALUE = "CrossingMinds/" + API_VERSION + " (Java/" + JAVA_VERSION
-			+ "; JSON)";
+	public static final String HEADER_USER_AGENT_VALUE = "CrossingMinds/" + clientVersion;
 	public static final String HEADER_CONTENT_TYPE = "Content-Type";
 	public static final String HEADER_CONTENT_TYPE_JSON_VALUE = "application/json";
 	public static final String HEADER_ACCEPT = "Accept";
@@ -35,17 +36,22 @@ public class Constants {
 	/**
 	 * Related to EndPoints
 	 */
-	public static final String ENDPOINT_LIST_ALL_ACCOUNTS = API_URL + "organizations/accounts/";
-	public static final String ENDPOINT_CREATE_INDIVIDUAL_ACCOUNT = API_URL + "accounts/individual/";
-	public static final String ENDPOINT_DELETE_INDIVIDUAL_ACCOUNT = API_URL + "accounts/individual/";
-	public static final String ENDPOINT_CREATE_SERVICE_ACCOUNT = API_URL + "accounts/service/";
-	public static final String ENDPOINT_DELETE_SERVICE_ACCOUNT = API_URL + "accounts/service/";
-	public static final String ENDPOINT_LOGIN_INDIVIDUAL_ACCOUNT = API_URL + "login/individual/";
-	public static final String ENDPOINT_LOGIN_SERVICE_ACCOUNT = API_URL + "login/service/";
-	public static final String ENDPOINT_LOGIN_ROOT = API_URL + "login/root/";
-	public static final String ENDPOINT_RENEW_LOGIN_REFRESH_TOKEN = API_URL + "login/refresh-token/";
-	public static final String ENDPOINT_RESEND_EMAIL_VERIFICATION_CODE = API_URL + "accounts/resend-verification-code/";
-	public static final String ENDPOINT_VERIFY_EMAIL = API_URL + "accounts/verify/";
-	public static final String ENDPOINT_DELETE_CURRENT_ACCOUNT = API_URL + "accounts/";
+	public static final String ENDPOINT_LIST_ALL_ACCOUNTS = "organizations/accounts/";
+	public static final String ENDPOINT_CREATE_INDIVIDUAL_ACCOUNT = "accounts/individual/";
+	public static final String ENDPOINT_DELETE_INDIVIDUAL_ACCOUNT = "accounts/individual/";
+	public static final String ENDPOINT_CREATE_SERVICE_ACCOUNT = "accounts/service/";
+	public static final String ENDPOINT_DELETE_SERVICE_ACCOUNT = "accounts/service/";
+	public static final String ENDPOINT_LOGIN_INDIVIDUAL_ACCOUNT = "login/individual/";
+	public static final String ENDPOINT_LOGIN_SERVICE_ACCOUNT = "login/service/";
+	public static final String ENDPOINT_LOGIN_ROOT = "login/root/";
+	public static final String ENDPOINT_RENEW_LOGIN_REFRESH_TOKEN = "login/refresh-token/";
+	public static final String ENDPOINT_RESEND_EMAIL_VERIFICATION_CODE = "accounts/resend-verification-code/";
+	public static final String ENDPOINT_VERIFY_EMAIL = "accounts/verify/";
+	public static final String ENDPOINT_DELETE_CURRENT_ACCOUNT = "accounts/";
+	public static final String ENDPOINT_CREATE_DATABASE = "databases/";
 
+	/**
+	 * Related to literals
+	 */
+	public static final String UNKNOWN_ERROR_MSG = "Unknown error from server";
 }
