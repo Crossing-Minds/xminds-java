ee38cfe26b98985d9f1ec12679835f0c940d7e40
Julio Briceno <ing.juliob@gmail.com>
2020-10-07 13:00:00 -0300

    Adapt to handle pre-mapped Xmind errors

--- xminds/src/main/java/com/crossingminds/xminds/api/Parser.java
+++ xminds/src/main/java/com/crossingminds/xminds/api/Parser.java
@@ -1,7 +1,5 @@
 package com.crossingminds.xminds.api;
 
-import java.util.Map;
-
 import com.crossingminds.xminds.api.exception.AuthenticationException;
 import com.crossingminds.xminds.api.exception.DuplicatedException;
 import com.crossingminds.xminds.api.exception.ForbiddenException;
@@ -15,67 +13,78 @@ import com.crossingminds.xminds.api.exception.ServerUnavailableException;
 import com.crossingminds.xminds.api.exception.TooManyRequestsException;
 import com.crossingminds.xminds.api.exception.WrongDataException;
 import com.crossingminds.xminds.api.exception.XmindsException;
+import com.crossingminds.xminds.api.mapper.JSONObjectMapper;
 import com.crossingminds.xminds.api.model.Base;
+import com.crossingminds.xminds.api.model.BaseError;
 import com.crossingminds.xminds.api.model.Errors;
 
 public class Parser {
 
+	private Parser() {
+	}
+
 	/**
-	 * This method evaluate the response body of all requests
-	 * to identify handled errors
+	 * This method evaluate the response body of all requests to identify handled
+	 * errors
 	 * 
 	 * @param obj
 	 * @return the same object
 	 * @throws XmindsException
 	 */
 	public static Object parseResponse(Object obj) throws XmindsException {
-		Base base = (Base) obj;
-		if (base.getAdditionalProperties().containsKey("error_code")) {
-			String errorName = (String) base.getAdditionalProperties().get("error_name");
-			Errors errorDetails = Errors.valueOf(errorName);
-			Map<String, Object> errorData;
-			String error = "";
-			String type = "";
-			String key = "";
+		Base base = null;
+		try {
+			base = (Base) obj;
+		} catch(ClassCastException ex) {
+			JSONObjectMapper<BaseError> baseMapper = new JSONObjectMapper<>();
+			base = new Base();
+			base.setError(baseMapper.jsonToObject((String) obj, BaseError.class));
+		}
+		if (base.getError().getErrorName() != null) {
+			String errorName = base.getError().getErrorName();
+			Errors errorDetails = Errors.valueOf(errorName.toUpperCase());
+			var error = base.getError().getErrorData().getError();
+			var type = base.getError().getErrorData().getType();
+			var key = base.getError().getErrorData().getKey();
+			var method = base.getError().getErrorData().getMethod();
 			switch (errorName) {
-				case "ServerError":
-					throw new ServerException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "ServerUnavailable":
-					throw new ServerUnavailableException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "TooManyRequests":
-					throw new TooManyRequestsException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "AuthError":
-					errorData = (Map<String, Object>) base.getAdditionalProperties().get("error_data");
-					error = (String) errorData.get("error");
-					throw new AuthenticationException(errorDetails.getMsg().replace("{error}", error), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "JwtTokenExpired":
-					throw new JwtTokenExpiredException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "RefreshTokenExpired":
-					throw new RefreshTokenExpiredException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "RequestError":
-					throw new RequestException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "WrongData":
-					throw new WrongDataException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "DuplicatedError":
-					errorData = (Map<String, Object>) base.getAdditionalProperties().get("error_data");
-					type = (String) errorData.get("type");
-					key = (String) errorData.get("key");
-					throw new DuplicatedException(errorDetails.getMsg().replace("{type}", type).replace("{key", key), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "ForbiddenError":
-					errorData = (Map<String, Object>) base.getAdditionalProperties().get("error_data");
-					error = (String) errorData.get("error");
-					throw new ForbiddenException(errorDetails.getMsg().replace("{error}", error), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "NotFoundError":
-					errorData = (Map<String, Object>) base.getAdditionalProperties().get("error_data");
-					type = (String) errorData.get("type");
-					key = (String) errorData.get("key");
-					throw new NotFoundException(errorDetails.getMsg().replace("{type}", type).replace("{key", key), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				case "MethodNotAllowed":
-					errorData = (Map<String, Object>) base.getAdditionalProperties().get("error_data");
-					String method = (String) errorData.get("method");
-					throw new MethodNotAllowedException(errorDetails.getMsg().replace("{method}", method), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
-				default:
-					throw new ServerException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
+			case "ServerUnavailable":
+				throw new ServerUnavailableException(errorDetails.getMsg(), errorDetails.getCode(),
+						errorDetails.getHttpStatus(), 0);
+			case "TooManyRequests":
+				throw new TooManyRequestsException(errorDetails.getMsg(), errorDetails.getCode(),
+						errorDetails.getHttpStatus(), 0);
+			case "AuthError":
+				throw new AuthenticationException(errorDetails.getMsg().replace("{error}", error),
+						errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
+			case "JwtTokenExpired":
+				throw new JwtTokenExpiredException(errorDetails.getMsg(), errorDetails.getCode(),
+						errorDetails.getHttpStatus(), 0);
+			case "RefreshTokenExpired":
+				throw new RefreshTokenExpiredException(errorDetails.getMsg(), errorDetails.getCode(),
+						errorDetails.getHttpStatus(), 0);
+			case "RequestError":
+				throw new RequestException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(),
+						0);
+			case "WrongData":
+				throw new WrongDataException(errorDetails.getMsg(), errorDetails.getCode(),
+						errorDetails.getHttpStatus(), 0);
+			case "DuplicatedError":
+				throw new DuplicatedException(errorDetails.getMsg().replace("{type}", type).replace("{key}", key),
+						errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
+			case "ForbiddenError":
+				throw new ForbiddenException(errorDetails.getMsg().replace("{error}", error), errorDetails.getCode(),
+						errorDetails.getHttpStatus(), 0);
+			case "NotFoundError":
+				throw new NotFoundException(errorDetails.getMsg().replace("{type}", type).replace("{key}", key),
+						errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
+			case "MethodNotAllowed":
+				throw new MethodNotAllowedException(errorDetails.getMsg().replace("{method}", method),
+						errorDetails.getCode(), errorDetails.getHttpStatus(), 0);
+			case "ServerError":
+			default:
+				throw new ServerException(errorDetails.getMsg(), errorDetails.getCode(), errorDetails.getHttpStatus(),
+						0);
 			}
 		}
 		return obj;
